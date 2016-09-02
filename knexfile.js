9a52315bddb787f3c2aa35ed58ae4a2649b986fe
erikmura44 <erikmura44@gmail.com>
2016-09-02 12:32:59 -0600

    changes

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?sslmode=require',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
